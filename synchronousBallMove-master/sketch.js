eba9fc8dcaed077fe4d7ceca3a877364d79ee249
raamrakesh8808 <101191030+raamrakesh8808@users.noreply.github.com>
2022-06-17 18:59:24 +0530

    Add files via upload

---
+++ synchronousBallMove-master/sketch.js
@@ -0,0 +1,46 @@
+var Ball, database;
+var position;
+
+
+function setup(){
+
+  console.log(database);
+  createCanvas(500,500);
+  database=firebase.database()
+  Ball = createSprite(250,250,10,10);
+  Ball.shapeColor = "red";
+  database.ref("ball/position").on("value",readPosition)
+
+}
+
+function draw(){
+  background("white");
+  
+    if(keyDown(LEFT_ARROW)){
+      writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+      writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+      writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+      writePosition(0,+1);
+    }
+    drawSprites();
+  
+}
+
+function writePosition(x,y){
+ database.ref("ball/position").set({x:Ball.x+x,y:Ball.y+y})
+}
+
+function readPosition(data){
+ position=data.val()
+ Ball.x=position.x
+ Ball.y=position.y
+}
+
+function showError(){
+}
